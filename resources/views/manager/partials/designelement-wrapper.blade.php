1048d761a80ada822dbe03b95962201da1a69c2e
Christoph <yipsocom@googlemail.com>
2018-02-27 16:17:45 +0100

    Make Design-Elements deleteable/resizable

--- resources/views/manager/partials/designelement-wrapper.blade.php
+++ resources/views/manager/partials/designelement-wrapper.blade.php
@@ -1,4 +1,8 @@
 <div class="col-md-{{ $width or 12 }} cell row">
+	<div class="pull-right">
+		<a class="voyager-trash cell-delete"></a>
+		<a class="voyager-resize-full cell-resize"></a>
+	</div>
 	<input type="hidden" name="row[][column]" value="design_element">
 	<div class="content">
 		@if (isset($formfield))
