7f3cd708a70ae7556767d7890eb4d8ef0659660c
Christoph <yipsocom@googlemail.com>
2018-03-11 17:44:36 +0100

    StyleCI fixes

--- resources/database/migrations/2018_02_02_000000_create_bread_table.php
+++ resources/database/migrations/2018_02_02_000000_create_bread_table.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Schema;
 class CreateBreadTable extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    * Run the migrations.
+    *
+    * @return void
+    */
     public function up()
     {
         // Create table for storing breads
@@ -23,11 +23,11 @@ class CreateBreadTable extends Migration
             $table->string('icon')->nullable();
             $table->string('model_name')->nullable();
             $table->string('policy_name')->nullable();
-			$table->string('controller_name')->nullable();
-			$table->integer('browse_list')->nullable()->unsigned();
-			$table->integer('read_view')->nullable()->unsigned();
-			$table->integer('add_view')->nullable()->unsigned();
-			$table->integer('edit_view')->nullable()->unsigned();
+            $table->string('controller_name')->nullable();
+            $table->integer('browse_list')->nullable()->unsigned();
+            $table->integer('read_view')->nullable()->unsigned();
+            $table->integer('add_view')->nullable()->unsigned();
+            $table->integer('edit_view')->nullable()->unsigned();
             $table->timestamps();
         });
 
@@ -40,44 +40,44 @@ class CreateBreadTable extends Migration
             $table->timestamps();
 
             $table->foreign('bread_id')->references('id')->on('bread')
-                ->onUpdate('cascade')->onDelete('cascade');
+            ->onUpdate('cascade')->onDelete('cascade');
         });
 
-		// Create table for storing bread rows
+        // Create table for storing bread rows
         Schema::create('bread_rows', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('bread_view_id')->unsigned();
-			$table->string('field')->nullable();
-			$table->string('type');
-			$table->integer('order')->nullable()->unsigned();
-			$table->integer('width')->nullable()->unsigned();
-			$table->text('options')->nullable();
+            $table->string('field')->nullable();
+            $table->string('type');
+            $table->integer('order')->nullable()->unsigned();
+            $table->integer('width')->nullable()->unsigned();
+            $table->text('options')->nullable();
             $table->text('validation_rules')->nullable();
             $table->timestamps();
 
-			$table->foreign('bread_view_id')->references('id')->on('bread_views')
-                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('bread_view_id')->references('id')->on('bread_views')
+            ->onUpdate('cascade')->onDelete('cascade');
         });
 
         // Create table for Roles <-> Views Relationship
         /*Schema::create('bread_view_role', function (Blueprint $table) {
-            $table->integer('role_id')->unsigned()->index();
-            $table->foreign('role_id')->references('id')->on('roles');
-            $table->integer('bread_view_id')->unsigned()->index();
-            $table->foreign('bread_view_id')->references('id')->on('bread_views');
-        });*/
-    }
+        $table->integer('role_id')->unsigned()->index();
+        $table->foreign('role_id')->references('id')->on('roles');
+        $table->integer('bread_view_id')->unsigned()->index();
+        $table->foreign('bread_view_id')->references('id')->on('bread_views');
+    });*/
+}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('bread');
-        Schema::drop('bread_views');
-        Schema::drop('bread_rows');
-        /*Schema::drop('bread_view_role');*/
-    }
+/**
+* Reverse the migrations.
+*
+* @return void
+*/
+public function down()
+{
+    Schema::drop('bread');
+    Schema::drop('bread_views');
+    Schema::drop('bread_rows');
+    /*Schema::drop('bread_view_role');*/
+}
 }
