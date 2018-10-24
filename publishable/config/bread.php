d88ef08ffad582541946ed93d474cd5048c7fa17
Christoph Schweppe <info@cschweppe.de>
2018-10-25 00:29:05 +0200

    Remove color config var

--- publishable/config/bread.php
+++ publishable/config/bread.php
@@ -3,7 +3,6 @@
 //https://christoph-schweppe.gitbook.io/voyager-bread/getting-started/configurations
 return [
     'bread_path'    => storage_path('bread'),
-    'colors'        => 'material-basic',
     'bread_buttons' => [
         'save_edit'     => true,
         'save_new'      => true,
