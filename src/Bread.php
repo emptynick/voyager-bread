f3226ddab82013ecb87283030defdb919ccaeba8
Christoph Schweppe <info@cschweppe.de>
2018-10-31 00:29:40 +0100

    Remove conflict-markers

--- src/Bread.php
+++ src/Bread.php
@@ -29,23 +29,16 @@ class Bread
 
     public function getBread($slug)
     {
-<<<<<<< HEAD
         return $this->getBreads()->where('slug', $slug)->first();
-=======
->>>>>>> de50ea7f5d20f13f9654bb94a36a7ba4b2cf821d
     }
 
     public function getBreadByTable($table)
     {
-<<<<<<< HEAD
         return $this->getBreads()->where('table', $table)->first();
-=======
->>>>>>> de50ea7f5d20f13f9654bb94a36a7ba4b2cf821d
     }
 
     public function getBreads()
     {
-<<<<<<< HEAD
         if (!$this->breads) {
             $this->breads = \Cache::remember('breads', now()->addHours(24), function ()
             {
@@ -67,45 +60,31 @@ class Bread
         }
 
         return $this->breads;
-=======
->>>>>>> de50ea7f5d20f13f9654bb94a36a7ba4b2cf821d
     }
 
     public function hasBread($slug)
     {
-<<<<<<< HEAD
         return !is_null($this->getBread($slug));
-=======
->>>>>>> de50ea7f5d20f13f9654bb94a36a7ba4b2cf821d
     }
 
     public function hasBreadByTable($table)
     {
-<<<<<<< HEAD
         return !is_null($this->getBreadByTable($table));
-=======
->>>>>>> de50ea7f5d20f13f9654bb94a36a7ba4b2cf821d
     }
 
     public function saveBread($bread)
     {
-<<<<<<< HEAD
         $path = config('bread.bread_path').DIRECTORY_SEPARATOR.$bread->table.'.json';
         return file_put_contents($path, json_encode($bread, JSON_PRETTY_PRINT));
-=======
->>>>>>> de50ea7f5d20f13f9654bb94a36a7ba4b2cf821d
     }
 
     public function deleteBread($table)
     {
-<<<<<<< HEAD
         $path = config('bread.bread_path').DIRECTORY_SEPARATOR.$table.'.json';
         if (!file_exists($path)) {
             return false;
         }
 
         return unlink($path);
-=======
->>>>>>> de50ea7f5d20f13f9654bb94a36a7ba4b2cf821d
     }
 }
