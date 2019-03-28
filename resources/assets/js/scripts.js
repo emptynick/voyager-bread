c1da292bec256fb315ca805cca24013d2ec0a05b
Christoph Schweppe <info@cschweppe.de>
2019-03-28 14:34:48 +0100

    Make path absolute

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -13,7 +13,7 @@ let helper = [
 
 require('./vendor');
 
-var LanguageInput = require('./components/LanguageInput').default;
+import LanguageInput from './components/LanguageInput.vue';
 Vue.component('language-input', LanguageInput);
 
 import LanguagePicker from './components/LanguagePicker';
