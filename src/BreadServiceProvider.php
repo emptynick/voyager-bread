b823bd41cd5f8888f53be7826ec2aeffb067708e
Christoph Schweppe <info@cschweppe.de>
2019-03-29 11:10:22 +0100

    Cleanup #3

--- src/BreadServiceProvider.php
+++ src/BreadServiceProvider.php
@@ -83,44 +83,20 @@ class BreadServiceProvider extends ServiceProvider
     {
         $namespace = '\\Bread\\Http\\Controllers\\';
 
-        $router->group([
-            'as'     => 'bread.',
-            'prefix' => 'bread',
-        ], function () use ($namespace, $router) {
+        $router->group(['as' => 'bread.', 'prefix' => 'bread', ], function () use ($namespace, $router) {
             //Index
-            $router->get('/', [
-                'uses' => $namespace.'ManagerController@index',
-                'as'   => 'index',
-            ]);
+            $router->get('/', ['uses' => $namespace.'ManagerController@index', 'as' => 'index']);
             //Create
-            $router->get('{table}/create', [
-                'uses' => $namespace.'ManagerController@create',
-                'as'   => 'create',
-            ]);
+            $router->get('{table}/create', ['uses' => $namespace.'ManagerController@create', 'as' => 'create']);
             //Store
-            $router->post('/', [
-                'uses' => $namespace.'ManagerController@store',
-                'as'   => 'store',
-            ]);
+            $router->post('/', ['uses' => $namespace.'ManagerController@store', 'as' => 'store']);
             //Edit
-            $router->get('{table}/edit', [
-                'uses' => $namespace.'ManagerController@create',
-                'as'   => 'edit',
-            ]);
+            $router->get('{table}/edit', ['uses' => $namespace.'ManagerController@create', 'as' => 'edit']);
             //Delete
-            $router->delete('{id}', [
-                'uses' => $namespace.'ManagerController@destroy',
-                'as'   => 'delete',
-            ]);
+            $router->delete('{id}', ['uses' => $namespace.'ManagerController@destroy', 'as' => 'delete']);
             //Assets
-            $router->get('/styles.css', [
-                'uses' => $namespace.'AssetController@styles',
-                'as'   => 'styles',
-            ]);
-            $router->get('/scripts.js', [
-                'uses' => $namespace.'AssetController@scripts',
-                'as'   => 'scripts',
-            ]);
+            $router->get('/styles.css', ['uses' => $namespace.'AssetController@styles', 'as' => 'styles']);
+            $router->get('/scripts.js', ['uses' => $namespace.'AssetController@scripts', 'as' => 'scripts']);
         });
 
         //BREADs
